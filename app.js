abbc65d86b1567b5ab50ffce856a836963fdd089
Gowthamap <gowthamap55555@gmail.com>
2023-08-23 21:09:49 +0530

    feat: dynamically add data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const canvas = d3.select(".canva");
 
-var dataArray = [4, 15, 34];
+var dataArray = [4, 15, 34, 123, 13, 0];
 
 // add an svg element
 // const svg = canvas.append("svg").attr("width", 600).attr("height", 600);
@@ -11,11 +11,13 @@ const svg = canvas.select("svg");
 const rect = svg.selectAll("rect");
 
 rect
-  .attr("width", 24)
   .data(dataArray)
+  .enter()
+  .append("rect")
+  .attr("width", 24)
   .attr("fill", "orange")
   .attr("height", function (d) {
-    return d * 3;
+    return d * 2;
   })
 
   .attr("x", function (d, i) {
@@ -23,7 +25,7 @@ rect
     return i * 25;
   })
   .attr("y", function (d, i) {
-    return 100 - d * 3;
+    return 100 - d * 2;
   });
 
 console.log(rect);
