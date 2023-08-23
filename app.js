e982a4faf0916d97f03bd3c6454c27b8a3bb4dea
Gowthamap <gowthamap55555@gmail.com>
2023-08-23 19:27:21 +0530

    feat: add circle svg using d3

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// d3.selectAll("p").style("color", "brown");
+
+const canvas = d3.select(".canva");
+
+// add an svg element
+const svg = canvas.append("svg")
+  .attr("width", "500")
+  .attr("height", "250");
+
+svg.append("circle").attr("cx", "100")
+  .attr("cy", "100")
+  .attr("r", "50")
+  .attr("fill", "blue")
