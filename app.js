116cc57c92a4c04c2588c829ce34fa10b412dba2
Gowthamap <gowthamap55555@gmail.com>
2023-08-23 19:42:35 +0530

    feat: svg.append 'rect' --> rectangle

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ const canvas = d3.select(".canva");
 
 // add an svg element
 const svg = canvas.append("svg")
-  .attr("width", "500")
-  .attr("height", "250");
+  .attr("width", 400)
+  .attr("height", 250); 
 
 svg.append("circle").attr("cx", "100")
-  .attr("cy", "100")
-  .attr("r", "50")
+  .attr("cy", 100)
+  .attr("r", 50)
   .attr("fill", "blue")
+
+svg.append('rect')
+  .attr("width", 100)
+  .attr("height", 100)
+  .attr("x", 120)
+  .attr("y", 0)
+  .attr("fill", "green")
+  .attr("rx", 15)
+  .attr("ry", 15)
+
+svg.append("line")
+  .attr("x1", 129)
+  .attr("x2", 45)
+  .attr("y1", 100)
+  .attr("y2", 46)
+  .attr("stroke", "gray")
